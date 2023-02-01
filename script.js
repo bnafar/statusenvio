28bcc105ec38140d10cc0288f313b35cb089dbc1
bnafar <bnafar@saude.gov.br>
2023-01-31 22:59:28 -0300

    add secao qualifarsus no html

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ $('#bologna-list a').on('click', function (e) {
     e.preventDefault()
     $(this).tab('show')
   })
+
+  $('#bologna-list2 a').on('click', function (e2) {
+    e2.preventDefault()
+    $(this).tab('show')
+  })
